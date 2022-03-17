166531321a44e9445dcaa4cf2d7a351e7a70ad66
engkimbs <eng.kimbs@gmail.com>
2022-03-17 13:41:15 +0900

    6-6 UI update

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,66 @@ App = {
   web3Provider: null,
   contracts: {},
 	
-  init: function() {
-   
+  init: function() {``
+    $.getJSON('../real-estate.json', (data) => {
+      let list = $('#list');
+      let template = $('#template');
+
+      for(i = 0; i < data.length; ++i) {
+        template.find('img').attr('src', data[i].picture);
+        template.find('.id').text(data[i].id);
+        template.find('.type').text(data[i].type);``
+        template.find('.area').text(data[i].area);
+        template.find('.price').text(data[i].price);
+
+        list.append(template.html());
+      }
+    })
+
+    return App.initWeb3();
   },
 
   initWeb3: function() {
-	
+    if(typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
   },
 
   initContract: function() {
-		
+    $.getJSON('RealEstate.json', (data) => {
+      App.contracts.RealEstate = TruffleContract(data);
+      App.contracts.RealEstate.setProvider(App.web3Provider);
+    })
   },
 
   buyRealEstate: function() {	
+    let id = $('#id').val();
+    let name = $('#name').val();
+    let price = $('#price').val();
+    let age = $('#age').val();
 
+    ethereum.enable().then((accounts) => {
+      var account = accounts[0];
+
+      console.log(account)
+
+      App.contracts.RealEstate.deployed().then((instance) => {
+        var nameUtf8Encoded = utf8.encode(name);
+        return instance.buyRealEstate(id, web3.toHex(nameUtf8Encoded), age, { from: account, value: price });
+      }).then(function() {
+        $('#name').val('');
+        $('#age').val('');
+        $('#buyModal').modal('hide');
+      }).catch((error) => {
+        console.log(error.message);
+      })
+    })
   },
 
   loadRealEstates: function() {
@@ -31,4 +77,13 @@ $(function() {
   $(window).load(function() {
     App.init();
   });
+
+  $('#buyModal').on('show.bs.modal', function(e) {
+    let id = $(e.relatedTarget).parent().find('.id').text();
+    let price = web3.toWei(parseFloat($(e.relatedTarget).parent().find('.price').text() || 0), "ether");
+
+    $(e.currentTarget).find('#id').val(id);
+    $(e.currentTarget).find('#price').val(price);
+
+  })
 });
--- test/TestRealEstate.js
+++ test/TestRealEstate.js
@@ -11,4 +11,24 @@ contract('RealEstate', (accounts) => {
             assert.equal(owner.toUpperCase(), accounts[0].toUpperCase(), "owner is not the first account of ganache")
         })
     })
+
+    it("Purchasing real estate id 0 via second ganache account and emit event, 'buyers' variable test", () => {
+        return RealEstate.deployed().then((instance) => {
+            realEstateInstance = instance;
+            return realEstateInstance.buyRealEstate(0, "sejong", 13, { from: accounts[1], value: web3.utils.toWei('1.5', "ether")});
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, "Event is not created.");
+            assert.equal(receipt.logs[0].event, "LogBuyRealEstate", "Event is not LogBuyRealEstate event");
+            assert.equal(receipt.logs[0].args._buyer, accounts[1], "Buyer is not second ganache account");
+            assert.equal(receipt.logs[0].args._id, 0, "Real estate's id is not 0");
+            return realEstateInstance.getBuyerInfo(0);
+        }).then((buyerInfo) => {
+            assert.equal(buyerInfo[0].toUpperCase(), accounts[1].toUpperCase(), "Buyer's account is not second ganache account");
+            assert.equal(buyerInfo[1], "sejong", "Buyer's name is not 'sejong'");
+            assert.equal(buyerInfo[2], 13, "Buyer's age is not 13");
+            return realEstateInstance.getAllBuyers();
+        }).then((buyers) => {
+            assert.equal(buyers[0].toUpperCase(), accounts[1].toUpperCase(), "The first index of 'buyers' is not second ganache account");
+        })
+    })
 })
